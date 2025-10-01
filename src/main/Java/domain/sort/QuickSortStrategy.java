1e982cbf4ba42b760a2aab32014f5e53090bc190
Sergei Babaian <sergey1302@yandex.ru>
2025-10-01 15:38:37 +0300

    added BubbleSortStrategiy and QuickSortStrategy

---
+++ src/main/Java/domain/sort/QuickSortStrategy.java
@@ -0,0 +1,44 @@
+package domain.sort;
+
+import data.repository.ArrayListToSortByStrategy;
+import domain.interfaces.SortStrategy;
+
+import java.util.Comparator;
+
+public class QuickSortStrategy implements SortStrategy {
+    @Override
+    public <T> ArrayListToSortByStrategy<T> sort(ArrayListToSortByStrategy<T> array, Comparator<T> comparator) {
+        return quickSort(array, 0, array.size() - 1, comparator);
+    }
+
+    <T> ArrayListToSortByStrategy<T> quickSort(ArrayListToSortByStrategy<T> array, int leftBorder, int rightBorder, Comparator<T> comparator) {
+        int leftMarker = leftBorder;
+        int rightMarker = rightBorder;
+        T pivot = array.get((leftMarker + rightMarker) / 2);
+        do {
+            while (comparator.compare(array.get(leftMarker), pivot) < 0) {
+                leftMarker++;
+            }
+            while (comparator.compare(array.get(rightMarker), pivot) > 0) {
+                rightMarker--;
+            }
+            if (leftMarker <= rightMarker) {
+                if (leftMarker < rightMarker) {
+                    T tmp = array.get(leftMarker);
+                    array.set(leftMarker, array.get(rightMarker));
+                    array.set(rightMarker, tmp);
+                }
+                leftMarker++;
+                rightMarker--;
+            }
+        } while (leftMarker <= rightMarker);
+
+        if (leftMarker < rightBorder) {
+            quickSort(array, leftMarker, rightBorder, comparator);
+        }
+        if (leftBorder < rightMarker) {
+            quickSort(array, leftBorder, rightMarker, comparator);
+        }
+        return array;
+    }
+}
