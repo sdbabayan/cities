0bec1e493a6a5355476e6c004f2ed280118ad36a
dima <dimon.stolbov@mail.ru>
2025-10-01 13:44:00 +0300

    interface SortStrategy<T>

--- src/main/java/examples/data/repository/ArrayListToSortByStrategy.java
+++ src/main/java/examples/data/repository/ArrayListToSortByStrategy.java
@@ -5,8 +5,8 @@ import examples.domain.interfaces.SortStrategy;
 import java.util.ArrayList;
 import java.util.Comparator;
 
-public class MyArrayList<T> extends ArrayList<T> {
-    public MyArrayList<T> sortByStrategy(SortStrategy<T> sortStrategy, Comparator<T> comparator) {
+public class ArrayListToSortByStrategy<T> extends ArrayList<T> {
+    public ArrayListToSortByStrategy<T> sortByStrategy(SortStrategy<T> sortStrategy, Comparator<T> comparator) {
         System.out.printf("Сортировка коллекции в соответствии со стратегией %s с компаратором %s:\n",
                 sortStrategy.getClass().getSimpleName(), comparator.getClass().getSimpleName());
         return sortStrategy.sort(this, comparator);
